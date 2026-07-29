fe9e0369d10a484545fe6ec972be57763a274091
Pooja <kusumpooja18@gmail.com>
2026-07-29 22:32:00 +0530

    Creating Fadein

--- frontend/components/common/FadeIn.tsx
+++ frontend/components/common/FadeIn.tsx
@@ -1,22 +1,28 @@
 "use client";
 
+
 import { motion } from "framer-motion";
 import { ReactNode } from "react";
 
 interface FadeInProps {
   children: ReactNode;
+  delay?: number;
 }
 
-export default function FadeIn({ children }: FadeInProps) {
+export default function FadeIn({ children, delay = 0 }: FadeInProps) {
   return (
+    
     <motion.div
-      initial={{ opacity: 0, y: 40 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{
+    initial={{ opacity: 0, y: 40 }}
+    whileInView={{ opacity: 1, y: 0 }}
+    viewport={{ once: true }}
+    transition={{
         duration: 0.8,
-      }}
-    >
-      {children}
-    </motion.div>
+        delay,
+    }}
+>
+    {children}
+</motion.div>
+
   );
 }
--- frontend/components/landing/Features.tsx
+++ frontend/components/landing/Features.tsx
@@ -1,26 +1,31 @@
 import { features } from "@/constants/features";
 import FeatureCard from "./FeatureCard";
 import SectionHeading from "../common/SectionHeading";
+import FadeIn from "../common/FadeIn";
+
 
 export default function Features() {
   return (
     <section className="bg-slate-950 py-24">
   <div className="mx-auto max-w-7xl px-6">
 
+     <FadeIn>
     <SectionHeading
-      title="Why Choose VirtualFit AI"
-      description="AI-powered features designed to transform your online fashion experience."
+        title="Why Choose VirtualFit AI"
+        description="AI-powered features designed to transform your online fashion experience."
     />
+      </FadeIn>
 
     <div className="mt-16 grid gap-8 md:grid-cols-2 lg:grid-cols-3">
-      {features.map((feature) => (
+       {features.map((feature, index) => (
+       <FadeIn key={feature.title} delay={index * 0.15}>
         <FeatureCard
-          key={feature.title}
-          title={feature.title}
-          description={feature.description}
-          icon={feature.icon}
+            title={feature.title}
+            description={feature.description}
+            icon={feature.icon}
         />
-      ))}
+    </FadeIn>
+))}
     </div>
 
   </div>
